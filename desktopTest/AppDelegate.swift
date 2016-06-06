e4342ff60eef0fcc42650072373f322e82828ec8
kris <kristjan.cocev@rettro.si>
2016-06-06 17:52:14 +0200

    Initial Commit

---
+++ desktopTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  desktopTest
+//
+//  Created by Joze Košmerl on 06/06/16.
+//  Copyright © 2016 Joze Košmerl. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ desktopTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  desktopTest
+//
+//  Created by Joze Košmerl on 06/06/16.
+//  Copyright © 2016 Joze Košmerl. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
